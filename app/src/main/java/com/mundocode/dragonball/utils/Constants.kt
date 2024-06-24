d7950935418eaf6c55c605de8c5858ab0f675b25
Steep <stevesalvadorman@gmail.com>
2024-06-23 21:41:52 -0500

    Fuente de dbz agregada al TopBar

--- app/src/main/java/com/mundocode/dragonball/utils/Constants.kt
+++ app/src/main/java/com/mundocode/dragonball/utils/Constants.kt
@@ -1,7 +1,21 @@
 package com.mundocode.dragonball.utils
 
+import androidx.compose.ui.graphics.Color
+
 object Constants {
-
     const val BASE_URL = "https://dragonball-api.com/api/"
-
+}
+object LetterColors {
+    val lettersWithColors = listOf(
+        "D" to Color(0xFFFFFF00),
+        "r" to Color(0xFFFFFF00),
+        "a" to Color(0xFFFFFF00),
+        "g" to Color(0xFFFFFF00),
+        "o" to Color(0xFFFF9800),
+        "n" to Color(0xFFFFFF00),
+        "B" to Color(0xFFFF0000),
+        "a" to Color(0xFFFF0000),
+        "l" to Color(0xFFFF0000),
+        "l" to Color(0xFFFF0000)
+    )
 }
